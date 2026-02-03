73d2611f8523212d9a0c351cb75eedff157e3341
Baruch Lopez <36717269+OBaruch@users.noreply.github.com>
2026-02-02 21:44:39 -0600

    Rename project to AirBaruch and clean structure

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Sincere Apology
 
-I want to express my deepest apologies for choosing the name "al_qaeda" for this project during my early college years, when I was yunger, Over time, I have come to understand the gravity and historical weight of this name, and I recognize that my choice was insensitive and inappropriate.
+I want to express my deepest apologies for choosing the name "al_qaeda" for this project during my early college years, when I was yunger. Over time, I have come to understand the gravity and historical weight of this name, and I recognize that my choice was insensitive and inappropriate. The project has been renamed to AirBaruch to reflect a more appropriate identity.
 
 I sincerely regret any pain, offense, or misunderstanding that this may have caused. It was never my intention to hurt anyone or to disrespect the victims of the tragic events associated with this term. I am committed to learning from this mistake and to moving forward with greater responsibility and sensitivity in all my future endeavors.
 
---
+++ src/AirBaruch/DijkstraCosto.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirBaruch
+{
+    [Serializable]
+    public class DijkstraCosto
+    {
+        Grafo g;
+        Nodo nInicial;
+        int infinito = 0;
+        int pesoDefi = 0;
+        bool conex = false;
+        
+
+        List<ElementoDijkstra> vD = new List<ElementoDijkstra>();
+
+        List<string> vDFinal = new List<string>();
+        Dictionary<Nodo, int> nodeIndex = new Dictionary<Nodo, int>();
+        Dictionary<Nodo, ElementoDijkstra> nodeElementMap = new Dictionary<Nodo, ElementoDijkstra>();
+
+        public DijkstraCosto(Grafo g, char inicio, bool conex)
+        {
+            this.conex = conex;
+            vD.Clear();
+            this.g = g;
+            Inicio(inicio);
+            iniVecDij();
+            generarDijkstra();
+            resFinal();
+            if (conex)
+                conexiones();
+        }
+        private void Inicio(char c)
+        {
+            foreach (Nodo n in g.getListaNodos())
+            {
+                if (n.getCiudad().getNom() == c.ToString())
+                {
+                    nInicial = n;
+                    break;
+                }
+            }
+
+        }
+
+        private void iniVecDij()
+        {
+            var nodos = g.getListaNodos();
+            infinito = 0;
+            nodeIndex.Clear();
+            nodeElementMap.Clear();
+            foreach (Nodo n in nodos)
+            {
+                n.getElementoD().serReiniciado();
+                nodeIndex[n] = nodeIndex.Count;
+                nodeElementMap[n] = n.getElementoD();
+            }
+                
+
+            foreach (Nodo n in nodos)
+            {
+                vD.Add(n.getElementoD());
+                foreach (Ady ady in n.getListaAdyacencia())
+                {
+                    infinito += ady.getPonderacionCosto();
+                }
+            }
+            foreach (ElementoDijkstra e in vD)
+            {
+                if (nInicial == e.getn())
+                    e.setPeso(0);
+                else
+                    e.setPeso(infinito);
+            }
+
+        }
+        private void generarDijkstra()
+        {
+            bool bandera = true;
+            while (bandera)
+            {
+                ElementoDijkstra e = seleccionaVD();
+                e.setDefinitivo();
+                pesoDefi = e.getPeso();
+                actualizarPesos(e);
+                bandera = checarBandera();
+            }
+        }
+        private ElementoDijkstra seleccionaVD()
+        {
+            ElementoDijkstra posible = vD[0];
+            int pesoM = infinito;
+
+            foreach (ElementoDijkstra e in vD)
+            {
+                if (e.getDefinitivo() == false)
+                {
+                    if (e.getPeso() < pesoM)
+                    {
+                        pesoM = e.getPeso();
+                        posible = e;
+                    }
+                }
+            }
+            return posible;
+        }
+        private void actualizarPesos(ElementoDijkstra e)
+        {
+            Nodo nID = e.getn();
+            foreach (Ady ady in nID.getListaAdyacencia())
+            {
+                Nodo nAdy = ady.getNodo();
+                if (nAdy.getElementoD().getDefinitivo() == false)
+                {
+                    int indexAdy = nodeIndex[nAdy];
+                    if (ady.getPonderacionCosto() + pesoDefi < vD[indexAdy].getPeso())
+                    {
+                        vD[indexAdy].setProveniente(nID, ady.getPonderacionCosto() + pesoDefi);
+                    }
+                }
+
+            }
+
+        }
+        private bool checarBandera()
+        {
+            foreach (ElementoDijkstra e in vD)
+            {
+                if (e.getDefinitivo() != true && e.getPeso() != infinito)
+                    return true;
+            }
+            return false;
+        }
+        private void resFinal()
+        {
+            for (int i = 0; i < vD.Count(); i++)
+            {
+                if (vD[i].getPeso() != infinito)
+                {
+                    string nueFinal = Convert.ToString(vD[i].getPeso()) + "<-" + vD[i].getn().getCiudad().getNom();
+                    ElementoDijkstra x = vD[i];
+                    while (x.getProveniente() != null)
+                    {
+                        ElementoDijkstra siguiente = nodeElementMap[x.getProveniente()];
+                        nueFinal += "<-" + siguiente.getn().getCiudad().getNom();
+                        x = siguiente;
+                    }
+                    vDFinal.Add(nueFinal);
+                }
+            }
+        }
+        //Publicos---------------------------
+        public List<string> getvD()
+        {
+            return vDFinal;
+        }
+        public void conexiones()
+        {
+            for (int i = 0; i < vD.Count(); i++)
+            {
+                if (vD[i].getProveniente() != null)
+                {
+                    vD[i].getProveniente().setPrimKruskal(vD[i].getn().getCiudad().getNom());
+                }
+            }
+        }
+    }
+}
